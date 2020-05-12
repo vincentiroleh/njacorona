4758552681e57c2907876f765665274592af7b5b
Vincent Iroleh <irolehiroleh@gmail.com>
2020-05-12 15:32:58 +0100

    implemented API

--- public/app.js
+++ public/app.js
@@ -1,55 +1,52 @@
-// dummy data
-const data = [
-    {
-        state: "Abia State",
-        report: 2
-    },
-    {
-        state: "Imo State",
-        report: 3
-    },
-    {
-        state: "Anambra State",
-        report: 1
-    },
-    {
-        state: "Enugu State",
-        report: 10
-    },
-    {
-        state: "Ebonyi State",
-        report: 7
-    }
-];
+// fetch API
+const url = "https://covidnigeria.herokuapp.com/api";
+fetch(url)
+    .then((response) => response.json())
+    .then((response) => {
+        // declare required variables
+        const nigeria = response.data;
+        const abia = response.data.states[33];
+        const imo = response.data.states[31];
+        const enugu = response.data.states[27];
+        const ebonyi = response.data.states[28];
 
-// get our elements
-const abia = document.querySelector('.abia');
-const abiaReport = document.querySelector('.abia__report');
+        // get elements
+        const el_nigeria = document.querySelector('.nigeria');
+        const el_nigeria_confirmed_cases = document.querySelector('.nigeria_confirmed_cases');
+        const el_nigeria_active_cases = document.querySelector('.nigeria_active_cases');
+        const el_nigeria_discharged_cases = document.querySelector('.nigeria_discharged_cases');
+        const el_nigeria_death_cases = document.querySelector('.nigeria_death_cases');
 
-const imo = document.querySelector('.imo');
-const imoReport = document.querySelector('.imo__report');
+        const el_abia = document.querySelector('.abia')
+        const el_abiaReport = document.querySelector('.abia__report')
 
-const anambra = document.querySelector('.anambra');
-const anambraReport = document.querySelector('.anambra__report');
+        const el_imo = document.querySelector('.imo');
+        const el_imoReport = document.querySelector('.imo__report')
 
-const enugu = document.querySelector('.enugu');
-const enuguReport = document.querySelector('.enugu__report');
+        const el_enugu = document.querySelector('.enugu')
+        const el_enuguReport = document.querySelector('.enugu__report')
 
-const ebonyi = document.querySelector('.ebonyi');
-const ebonyiReport = document.querySelector('.ebonyi__report');
+        const el_ebonyi = document.querySelector('.ebonyi')
+        const el_ebonyiReport = document.querySelector('.ebonyi__report')
 
-// change element contents
-abia.textContent = data[0].state;
-abiaReport.textContent = data[0].report;
+        // append data from api
+        el_nigeria.textContent = 'Nigeria';
+        el_nigeria_confirmed_cases.textContent = nigeria.totalConfirmedCases;
+        el_nigeria_active_cases.textContent = nigeria.totalActiveCases;
+        el_nigeria_discharged_cases.textContent = nigeria.discharged;
+        el_nigeria_death_cases.textContent = nigeria.death
 
-imo.textContent = data[1].state;
-imoReport.textContent = data[1].report;
 
-anambra.textContent = data[2].state;
-anambraReport.textContent = data[2].report;
+        el_abia.textContent = abia.state;
+        el_abiaReport.textContent = abia.confirmedCases;
 
-enugu.textContent = data[3].state;
-enuguReport.textContent = data[3].report;
+        el_imo.textContent = imo.state;
+        el_imoReport.textContent = imo.confirmedCases;
 
-ebonyi.textContent = data[4].state;
-ebonyiReport.textContent = data[4].report;
+        el_enugu.textContent = enugu.state;
+        el_enuguReport.textContent = enugu.confirmedCases;
+
+        el_ebonyi.textContent = ebonyi.state;
+        el_ebonyiReport.textContent = ebonyi.confirmedCases;
+    })
+    .catch(err => console.log(err))
