d5f05dfe5ca41e5924bf36bb313a1ccef7c8f240
Vincent Iroleh <irolehiroleh@gmail.com>
2020-05-13 15:08:43 +0100

    updated the logic

--- public/app.js
+++ public/app.js
@@ -5,10 +5,45 @@ fetch(url)
     .then((response) => {
         // declare required variables
         const nigeria = response.data;
-        const abia = response.data.states[33];
-        const imo = response.data.states[31];
-        const enugu = response.data.states[27];
-        const ebonyi = response.data.states[28];
+        const section = document.querySelector('.states');
+
+        const states = response.data.states;
+        states.forEach(state => {
+            // create div, h6 and p el
+            const div1 = document.createElement('div');
+            const card = document.createElement('div');
+            const cardHeader = document.createElement('div');
+            const cardBody = document.createElement('div');
+            const cardTitle = document.createElement('h6');
+            const cardText = document.createElement('p');
+
+            // add classes to them
+            div1.className = 'col-sm-6';
+            card.className = 'card mb-3 text-white border border-light rounded';
+            cardHeader.className = 'card-header bg-secondary'
+            cardBody.className = 'card-body bg-dark ';
+            cardTitle.className = 'card-title';
+            cardText.className = 'card-text btn btn-outline-primary'
+
+            // append content form our api
+            cardHeader.appendChild(document.createTextNode(state.state));
+            cardTitle.textContent = 'Confirmed';
+            cardText.appendChild(document.createTextNode(state.confirmedCases));
+            
+            // append to each other
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText)
+
+            card.appendChild(cardHeader);
+            card.appendChild(cardBody);
+
+            div1.appendChild(card);
+
+
+            section.appendChild(div1);
+            
+        });
+        
 
         // get elements
         const el_nigeria = document.querySelector('.nigeria');
@@ -17,18 +52,6 @@ fetch(url)
         const el_nigeria_discharged_cases = document.querySelector('.nigeria_discharged_cases');
         const el_nigeria_death_cases = document.querySelector('.nigeria_death_cases');
 
-        const el_abia = document.querySelector('.abia')
-        const el_abiaReport = document.querySelector('.abia__report')
-
-        const el_imo = document.querySelector('.imo');
-        const el_imoReport = document.querySelector('.imo__report')
-
-        const el_enugu = document.querySelector('.enugu')
-        const el_enuguReport = document.querySelector('.enugu__report')
-
-        const el_ebonyi = document.querySelector('.ebonyi')
-        const el_ebonyiReport = document.querySelector('.ebonyi__report')
-
         // append data from api
         el_nigeria.textContent = 'Nigeria';
         el_nigeria_confirmed_cases.textContent = nigeria.totalConfirmedCases;
@@ -36,17 +59,5 @@ fetch(url)
         el_nigeria_discharged_cases.textContent = nigeria.discharged;
         el_nigeria_death_cases.textContent = nigeria.death
 
-
-        el_abia.textContent = abia.state;
-        el_abiaReport.textContent = abia.confirmedCases;
-
-        el_imo.textContent = imo.state;
-        el_imoReport.textContent = imo.confirmedCases;
-
-        el_enugu.textContent = enugu.state;
-        el_enuguReport.textContent = enugu.confirmedCases;
-
-        el_ebonyi.textContent = ebonyi.state;
-        el_ebonyiReport.textContent = ebonyi.confirmedCases;
     })
     .catch(err => console.log(err))
